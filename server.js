c1c3d1c64575ee0002767382d979040fefdc4525
CodeSevenic <sevenicbookings@gmail.com>
2023-06-26 21:45:13 +0200

    Hub-Nabled

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-﻿
+﻿require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
