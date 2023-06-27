f7fa64b1ee97fcd6d59549a78c72614cf26dd1d5
CodeSevenic <sevenicbookings@gmail.com>
2023-06-27 15:03:31 +0200

    Hub-Nabled

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the server' });
 });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
