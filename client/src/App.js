8c2653d44bd893b75f3bde9ae051b72e78477d42
CodeSevenic <sevenicbookings@gmail.com>
2023-06-27 16:12:03 +0200

    Hub-Nabled

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <div className="main"></div>
       <h1>hello world</h1>
     </div>
   );
