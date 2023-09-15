7c5bfb92a49d2dc5fcde09f4df13cb08855f1df8
CodeSevenic <sevenicbookings@gmail.com>
2023-09-15 09:27:53 +0200

    KasiChat Code Update

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -7,11 +7,18 @@ const authCtrl = {
     try {
       const { fullname, username, email, password, gender } = req.body;
       let newUserName = username.toLowerCase().replace(/ /g, '');
-
+      // check if username and email already exists
       const user_name = await Users.findOne({ username: newUserName });
       if (user_name) return res.status(400).json({ msg: 'This username already exists.' });
       const user_email = await Users.findOne({ email });
-      if (user_name) return res.status(400).json({ msg: 'This email already exists.' });
+      if (user_email) return res.status(400).json({ msg: 'This email already exists.' });
+      // check if password is at least 6 characters long
+      if (password.length < 6)
+        return res.status(400).json({ msg: 'Password must be at least 6 characters long.' });
+
+      // Password Encryption
+      const passwordHash = await bcrypt.hash(password, 12);
+
       console.log(newUserName);
       return res.json({ msg: 'Successfully Registered!' });
     } catch (err) {
