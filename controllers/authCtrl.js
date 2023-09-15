f59355bad05768aabace0aba197628784a8b602d
CodeSevenic <sevenicbookings@gmail.com>
2023-09-15 08:33:32 +0200

    KasiChat Code Update

---
+++ controllers/authCtrl.js
@@ -0,0 +1,12 @@
+﻿const Users = require('../models/usersModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const authCtrl = {
+  register: async (req, res) => {
+    try {
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
