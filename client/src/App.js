aa8370a5326f2d4e95da823e6e4993888cb12b42
CodeSevenic <sevenicbookings@gmail.com>
2023-09-15 07:51:52 +0200

    KasiChat Code Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PageRender from './PageRender';
 
 function App() {
   return (
-    <div className="App">
-      <div className="main"></div>
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <input type="checkbox" name="theme" id="theme" />
+      <div className="App">
+        <div className="main">
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/PageRender.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import { useParams } from 'react-router-dom';
+import NotFound from './components/NotFound';
+
+// This function generates a page component based on the page name
+const generatePage = (pageName) => {
+  // Dynamically import the component using the page name
+  const component = () => require(`./pages/${pageName}`).default;
+  try {
+    // Create an element using the imported component
+    return React.createElement(component());
+  } catch (err) {
+    // If there's an error, log it and return the NotFound component
+    console.warn(err);
+    return <NotFound />;
+  }
+};
+
+// This is the main PageRender component
+const PageRender = () => {
+  // Get the page and id parameters from the URL
+  const { page, id } = useParams();
+  let pageName = '';
+  // If there's an id parameter, use the [id] version of the page name
+  if (id) {
+    pageName = `${page}/[id]`;
+  } else {
+    // Otherwise, use the regular page name
+    pageName = `${page}`;
+  }
+  // Generate the page component and return it
+  return generatePage(pageName);
+};
+
+export default PageRender;
