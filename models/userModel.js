b6adb90e50ae76340b4c5152519024cc7f900b44
CodeSevenic <sevenicbookings@gmail.com>
2023-09-15 08:19:13 +0200

    KasiChat Code Update

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,40 @@
-﻿
+﻿const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 25,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 25,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/dq7l8216n/image/upload/v1628580399/avatar/avatar_cugq40.png',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('user', userSchema);
