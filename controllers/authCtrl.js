cdfbfb24e16415c199b66f5a3fc13ed1273e901a
CodeSevenic <sevenicbookings@gmail.com>
2023-09-16 14:16:57 +0200

    KasiChat Code Update

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -95,6 +95,12 @@ const authCtrl = {
     try {
       const rf_token = req.cookies.refreshtoken;
       if (!rf_token) return res.status(400).json({ msg: 'Please login now!' });
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async (err, result) => {
+        if (err) return res.status(400).json({ msg: 'Please login now!' });
+        const user = await Users.findById(result.id)
+          .select('-password')
+          .populate('followers following', '-password');
+      });
       res.json({ rf_token });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
