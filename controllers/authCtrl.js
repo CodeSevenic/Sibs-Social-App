114390387377feed589ef32c6e774ab7c40ab67e
CodeSevenic <sevenicbookings@gmail.com>
2023-09-16 12:01:17 +0200

    KasiChat Code Update

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -56,4 +56,12 @@ const authCtrl = {
   },
 };
 
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+};
+
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' });
+};
+
 module.exports = authCtrl;
