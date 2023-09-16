220c46d5fa7822e35f7659409eb57d996795e44f
CodeSevenic <sevenicbookings@gmail.com>
2023-09-16 13:58:16 +0200

    KasiChat Code Update

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -51,6 +51,7 @@ const authCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+
   login: async (req, res) => {
     const { email, password } = req.body;
     const user = await Users.findOne({ email }).populate('followers following', '-password');
