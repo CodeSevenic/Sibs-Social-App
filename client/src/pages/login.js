9de0242887e6ede83acc177aa26f79c0b40f475e
CodeSevenic <sevenicbookings@gmail.com>
2023-09-17 09:50:12 +0200

    KasiChat Code Update

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,39 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  const initialState = { email: '', password: '' };
+  const [userData, setUserData] = useState(initialState);
+  const { email, password } = userData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+  return (
+    <div className="auth_page">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <small id="emailHelp" class="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
