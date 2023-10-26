fa11fdf854cf29b07d2b36b42b836b67366a9bee
CodeSevenic <sevenicbookings@gmail.com>
2023-10-26 21:08:58 +0200

    KasiChat Code Update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers/index';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const DataProvider = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
