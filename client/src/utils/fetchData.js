7459b971ef2499019a7647e0317b6557a0c0f89f
CodeSevenic <sevenicbookings@gmail.com>
2023-10-27 17:32:45 +0200

    KasiChat Code Update

---
+++ client/src/utils/fetchData.js
@@ -0,0 +1,8 @@
+﻿import axios from 'axios';
+
+export const getDataAPI = async (url, token) => {
+  const res = await axios.get(`/api/${url}`, {
+    headers: { Authorization: token },
+  });
+  return res;
+};
