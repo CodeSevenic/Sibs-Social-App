1fa59a363b2fbce97b907d6fa13379ab7431ec03
CodeSevenic <sevenicbookings@gmail.com>
2023-10-27 17:21:09 +0200

    KasiChat Code Update

--- client/src/redux/reducers/notifyReducer.js
+++ client/src/redux/reducers/notifyReducer.js
@@ -1,16 +1,11 @@
-﻿import { TYPES } from '../actions/authAction';
+﻿import { TYPES } from '../actions/notifyAction';
 
 const initialState = {};
 
 const notifyReducer = (state = initialState, action) => {
   switch (action.type) {
-    case TYPES.AUTH:
-      return {
-        ...state,
-        ...action.payload,
-        isAuthenticated: true,
-        isLoading: false,
-      };
+    case TYPES.NOTIFY:
+      return action.payload;
 
     default:
       return state;
