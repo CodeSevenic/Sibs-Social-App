c02c9e8fbeee95349c5ada8d170467670b24fd0e
CodeSevenic <sevenicbookings@gmail.com>
2023-10-27 21:53:55 +0200

    KasiChat Code Update

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -6,22 +6,23 @@ export const TYPES = {
 
 export const login = (data) => async (dispatch) => {
   try {
-    dispatch({
-      type: 'NOTIFY',
-      payload: {
-        loading: true,
-      },
-    });
+    dispatch({ type: 'NOTIFY', payload: { loading: true } });
 
     const res = await postDataAPI('login', data);
-
-    localStorage.setItem('firstLogin', true);
-
     dispatch({
       type: 'AUTH',
       payload: {
         token: res.data.access_token,
         user: res.data.user,
+      },
+    });
+
+    localStorage.setItem('firstLogin', true);
+
+    dispatch({
+      type: 'NOTIFY',
+      payload: {
+        success: res.data.msg,
       },
     });
   } catch (error) {
