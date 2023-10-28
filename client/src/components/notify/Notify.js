4d4f96967f018c868ed82bd273c8653739f6f3a1
CodeSevenic <sevenicbookings@gmail.com>
2023-10-28 09:44:13 +0200

    KasiChat Code Update

--- client/src/components/notify/Notify.js
+++ client/src/components/notify/Notify.js
@@ -1,10 +1,11 @@
 ﻿import React from 'react';
 import { useSelector } from 'react-redux';
+import Loading from './Loading';
 
 const Notify = () => {
   const state = useSelector((state) => state);
   const { notify } = state;
-  return <div>{notify.loading && <h1>Loading...</h1>}</div>;
+  return <div>{notify.loading && <Loading />}</div>;
 };
 
 export default Notify;
