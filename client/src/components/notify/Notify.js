f60d6913c33e263366de043272b74c444fabd1bc
CodeSevenic <sevenicbookings@gmail.com>
2023-12-07 16:09:27 +0200

    KasiChat Code Update

--- client/src/components/notify/Notify.js
+++ client/src/components/notify/Notify.js
@@ -5,11 +5,7 @@ import Loading from './Loading';
 const Notify = () => {
   const state = useSelector((state) => state);
   const { notify } = state;
-  return (
-    <div>
-      <Loading />
-    </div>
-  );
+  return <div>{notify.loading && <Loading />}</div>;
 };
 
 export default Notify;
