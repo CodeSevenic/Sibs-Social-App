06480f2c7e58bbedee91ed5866acd7cb3a9e946e
CodeSevenic <sevenicbookings@gmail.com>
2023-12-07 11:00:51 +0200

    KasiChat Code Update

--- client/src/components/notify/Loading.js
+++ client/src/components/notify/Loading.js
@@ -2,7 +2,10 @@
 
 const Loading = () => {
   return (
-    <div style={{ background: '#0008' }}>
+    <div
+      className="position-fixed w-100 h-100 text-center loading"
+      style={{ background: '#0008', color: 'white', top: 0, left: 0, zIndex: 50 }}
+    >
       <svg width="205" height="250" viewBox="0 0 40 50">
         <polygon strokeWidth="1" stroke="#fff" fill="none" points="20,1 40,40 1,40"></polygon>
       </svg>
