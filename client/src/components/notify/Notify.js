45e509c92aa671df03a79003d5faf43b21bc89ae
CodeSevenic <sevenicbookings@gmail.com>
2023-12-07 22:22:10 +0200

    KasiChat Code Update

--- client/src/components/notify/Notify.js
+++ client/src/components/notify/Notify.js
@@ -1,11 +1,27 @@
 ﻿import React from 'react';
 import { useSelector } from 'react-redux';
+
 import Loading from './Loading';
+import Toast from './Toast';
 
 const Notify = () => {
   const state = useSelector((state) => state);
   const { notify } = state;
-  return <div>{notify.loading && <Loading />}</div>;
+  return (
+    <div>
+      {notify.loading && <Loading />}
+      {notify.error && (
+        <Toast msg={{ title: 'Error', body: notify.error }} handleShow={''} bgColor="bg-danger" />
+      )}
+      {notify.success && (
+        <Toast
+          msg={{ title: 'Success', body: notify.success }}
+          handleShow={''}
+          bgColor="bg-success"
+        />
+      )}
+    </div>
+  );
 };
 
 export default Notify;
