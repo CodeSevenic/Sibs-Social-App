18b444faf2f25ea0daf11ea3070acca98d4c52a1
CodeSevenic <sevenicbookings@gmail.com>
2023-12-07 10:54:09 +0200

    KasiChat Code Update

--- client/src/components/notify/Loading.js
+++ client/src/components/notify/Loading.js
@@ -1,7 +1,13 @@
 ﻿import React from 'react';
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div style={{ background: '#0008' }}>
+      <svg width="205" height="250" viewBox="0 0 40 50">
+        <polygon strokeWidth="1" stroke="#fff" fill="none" points="20,1 40,40 1,40"></polygon>
+      </svg>
+    </div>
+  );
 };
 
 export default Loading;
--- client/src/components/notify/Notify.js
+++ client/src/components/notify/Notify.js
@@ -5,7 +5,11 @@ import Loading from './Loading';
 const Notify = () => {
   const state = useSelector((state) => state);
   const { notify } = state;
-  return <div>{notify.loading && <Loading />}</div>;
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
 };
 
 export default Notify;
