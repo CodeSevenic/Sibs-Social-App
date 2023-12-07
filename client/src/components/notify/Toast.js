bf4b12403d98778f251b493e3f1834f93aeb1f7f
CodeSevenic <sevenicbookings@gmail.com>
2023-12-07 21:56:08 +0200

    KasiChat Code Update

---
+++ client/src/components/notify/Toast.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+const Toast = () => {
+  return (
+    <div className="toast show position-fixed">
+      <div className="toast-header">
+        <strong>Toast Title</strong>
+        <button>&times</button>
+      </div>
+      <div className="toast-body">Toast body</div>
+    </div>
+  );
+};
+
+export default Toast;
