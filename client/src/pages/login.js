2621394aec285a014d81e3ee1dc005f81b75e31c
CodeSevenic <sevenicbookings@gmail.com>
2023-12-13 21:07:21 +0200

    KasiChat Code Update

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { login } from '../redux/actions/authAction';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
