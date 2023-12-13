588be93951e35c3b9375933535f33517b3e21ac3
CodeSevenic <sevenicbookings@gmail.com>
2023-12-13 20:43:44 +0200

    KasiChat Code Update

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,8 +1,85 @@
-﻿import React from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { login } from '../redux/actions/authAction';
 
 const Register = () => {
-  return <div>Register</div>;
+  const { auth } = useSelector((state) => state);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (auth.token) history.push('/');
+  }, [auth.token, history]);
+
+  const initialState = { email: '', password: '' };
+  const [userData, setUserData] = useState(initialState);
+  const { email, password } = userData;
+
+  const [typePass, setTypePass] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('on Submit: ', userData);
+    dispatch(login(userData));
+  };
+
+  return (
+    <div className="auth_page">
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-center mb-3">KasiChat</h3>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={handleChangeInput}
+            value={email}
+            name="email"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <div className="pass">
+            <input
+              type={typePass ? 'text' : 'password'}
+              className="form-control"
+              id="exampleInputPassword1"
+              onChange={handleChangeInput}
+              value={password}
+              name="password"
+            />
+            <small onClick={() => setTypePass(!typePass)}>{typePass ? 'Hide' : 'Show'}</small>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-dark w-100"
+          disabled={email && password ? false : true}
+        >
+          Submit
+        </button>
+        <p className="my-2">
+          You don't have an account?{' '}
+          <a href="/register" style={{ color: 'crimson' }}>
+            Register Now
+          </a>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
