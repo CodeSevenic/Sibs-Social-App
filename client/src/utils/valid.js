63b706bca6012a75f7a13f8f45a9adf825507285
CodeSevenic <sevenicbookings@gmail.com>
2023-12-14 14:05:52 +0200

    KasiChat Code Update

---
+++ client/src/utils/valid.js
@@ -0,0 +1,14 @@
+﻿const valid = ({ fullname, username, email, password, cf_password, gender }) => {
+  const err = {};
+
+  if (!fullname) {
+    err.fullname = 'Please add your full name.';
+  } else if (fullname.length > 25) {
+    err.fullname = 'Full name is up to 25 characters long.';
+  }
+  if (!username) {
+    err.username = 'Please add your username.';
+  } else if (username.length > 25) {
+    err.username = 'Full name is up to 25 characters long.';
+  }
+};
