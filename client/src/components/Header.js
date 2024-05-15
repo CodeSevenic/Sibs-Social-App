daf8f48f19f0c58f76e5fd8366b2b1d968008a22
CodeSevenic <sevenicbookings@gmail.com>
2024-05-15 11:47:02 +0200

    KasiChat Code Update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,20 +7,9 @@ const Header = () => {
         <a className="navbar-brand" href="#">
           KasiChat
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
+        <div className="menu">
+          <ul className="navbar-nav flex-row">
             <li className="nav-item active">
               <a className="nav-link" href="#">
                 Home <span className="sr-only">(current)</span>
@@ -36,7 +25,7 @@ const Header = () => {
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-                Dropdown
+                User
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a className="dropdown-item" href="#">
