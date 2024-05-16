72d5b90e615cc4309ebf963b43b8323eb5249e02
CodeSevenic <sevenicbookings@gmail.com>
2024-05-16 09:30:57 +0200

    KasiChat Code Update

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Menu from './Menu';
+import Search from './Search';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
-      <Link className="navbar-brand" to="/">
-        <strong className="navbar-brand p-0 m-0">KasiChat</strong>
-      </Link>
-      <Menu />
-    </nav>
+    <div className="header">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
+        <Link className="navbar-brand" to="/">
+          <strong className="navbar-brand p-0 m-0">KasiChat</strong>
+        </Link>
+        <Search />
+        <Menu />
+      </nav>
+    </div>
   );
 };
 
