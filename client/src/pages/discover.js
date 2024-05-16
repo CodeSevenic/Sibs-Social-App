67e792dc0a0141dab6400fa2c49f71f7621636b8
CodeSevenic <sevenicbookings@gmail.com>
2024-05-16 08:10:10 +0200

    KasiChat Code Update

---
+++ client/src/pages/discover.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Discover = () => {
+  return (
+    <div>
+      <h1>Discover</h1>
+      <p>Discover page content</p>
+    </div>
+  );
+};
+
+export default Discover;
