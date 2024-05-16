6b0ca379014579776b577702fdc521bfd606b4cb
CodeSevenic <sevenicbookings@gmail.com>
2024-05-16 09:37:05 +0200

    KasiChat Code Update

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+  const [search, setSearch] = useState('');
   return (
     <form className="search_form">
-      <input type="text" name="search" placeholder="Search" />
-      <button type="submit">Submit</button>
+      <input
+        type="text"
+        name="search"
+        value={search}
+        placeholder="Search"
+        onClick={(e) => setSearch(e.target.value.toLowerCase().replace(/ /g, ''))}
+      />
     </form>
   );
 };
