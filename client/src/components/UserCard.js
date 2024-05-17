ecc6320282df6f695f619d3f2c002f2578d0d7b4
CodeSevenic <sevenicbookings@gmail.com>
2024-05-17 08:33:27 +0200

    KasiChat Code Update

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Avatar from './Avatar';
 
 const UserCard = ({ user }) => {
-  return <div>UserCard</div>;
+  return (
+    <div className="d-flex p-2 align-item-center">
+      <Avatar src={user.avatar} alt={user.name} size={'big-avatar'} />
+      <div className="ml-1">
+        <span className="d-block">{user.username}</span>
+        <small style={{ opacity: 0.7 }}>{user.fullname}</small>
+      </div>
+    </div>
+  );
 };
 
 export default UserCard;
