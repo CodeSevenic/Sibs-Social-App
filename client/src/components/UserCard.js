e1bb51a6b5ed3681b2b60b762fa5a03682721ecb
CodeSevenic <sevenicbookings@gmail.com>
2024-05-18 19:17:49 +0200

    KasiChat Code Update

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Avatar from './Avatar';
 
-const UserCard = ({ user }) => {
+const UserCard = ({ user, border }) => {
   return (
-    <div className="d-flex p-2 align-item-center">
+    <div className={`d-flex p-2 align-item-center ${border}`}>
       <Avatar src={user.avatar} alt={user.name} size={'big-avatar'} />
       <div className="ml-1">
         <span className="d-block">{user.username}</span>
